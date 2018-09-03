725d8e55b77d2a5ae938acaa929ccaee883aaa6e
Austin Wheeler <awheels@me.com>
2018-09-03 15:56:43 -0400

    More progress on the final exercises

--- exercises/intro_exercises.rb
+++ exercises/intro_exercises.rb
@@ -41,3 +41,21 @@ hash_arr = {[1,2,3] => "one two three", [4,5,6] => "four five six"}
 # Exercise 11
 # My favorite API is the Ruby-Doc.org site for Ruby,
 # and guides.rubyonrails.org for rails.
+
+# Exercise 12
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contacts["Joe Smith"] = {
+  email: contact_data[0][0],
+  address: contact_data[0][1],
+  phone: contact_data[0][2]
+  }
+contacts["Sally Johnson"] = {
+  email: contact_data[1][0],
+  address: contact_data[1][1],
+  phone: contact_data[1][2]
+  }
+puts contacts
