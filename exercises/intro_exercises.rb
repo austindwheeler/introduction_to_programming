17c708f12d9df06c385540e52284cc91493bb6f4
Austin Wheeler <awheels@me.com>
2018-09-04 02:34:31 -0400

    Completed the intro to programming book.

--- exercises/intro_exercises.rb
+++ exercises/intro_exercises.rb
@@ -59,3 +59,62 @@ contacts["Sally Johnson"] = {
   phone: contact_data[1][2]
   }
 puts contacts
+
+# Exercise 13
+puts contacts["Joe Smith"][:email]
+puts contacts["Sally Johnson"][:phone]
+
+# Exercise 14
+contact_data = ["joe@email.com", "123 Main st.", "555-123-4567"]
+contacts = {"Joe Smith" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each do |name, hash|
+  fields.each do |field|
+    hash[field] = contact_data.shift
+  end
+end
+p contacts
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+fields = [:email, :address, :phone]
+
+contacts.each_with_index do |(name, hash), idx|
+  fields.each do |field|
+    hash[field] = contact_data[idx].shift
+  end
+end
+p contacts
+
+# Exercise 15
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if {|word| word.start_with?("s")}
+p arr
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if {|word| word.start_with?("s", "w")}
+p arr
+
+# Exercise 16
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+a = a.map {|str| str.split(" ")}
+a = a.flatten
+p a
+
+# Exercise 17
+hash1 = {shoes: "nike", "hat" => "adidas", :hoodie => true}
+hash2 = {"hat" => "adidas", :shoes => "nike", hoodie: true}
+
+if hash1 == hash2
+  puts "These hashes are the same!"
+else
+  puts "These hashes are not the same!"
+end
+# Will output "These hashes are the same!" because the hashes contain the same
+# keys and values.
